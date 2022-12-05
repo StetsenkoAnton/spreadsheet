a295a78237e79d7e4e4a6d113e1f0b6f713c49df
Anton Stetsenko <anton.stetsenko@dataart.com>
2022-12-05 16:09:13 +0100

    use app form server

--- server/index.mjs
+++ server/index.mjs
@@ -7,14 +7,18 @@ import { default as http } from "http";
 import { SPREADSHEET_EVENTS } from '../core/spreadsheet-events.js';
 import * as XLSX from "xlsx/xlsx.mjs";
 import * as fs from "fs";
+import path from "path";
 import express from "express";
 import API_V_1 from "./api/index.router.mjs";
 
 /* load 'fs' for readFile and writeFile support */
 XLSX.set_fs(fs);
-
-const indexFile = new URL('./html/index.html', import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//const indexFile = new URL('./html/index.html', import.meta.url);
+const indexFile = new URL('../dist/index.html', import.meta.url);
 const app = express();
+app.use(express.static(path.join(__dirname, '/../dist')));
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
@@ -34,8 +38,8 @@ io.on("connection", (socket) => {
 
     console.log('message: ' + msg);
   });
-  });
+});
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
+  console.log("listening on http://localhost:3000");
 });
