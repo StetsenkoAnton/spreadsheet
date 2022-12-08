0eeaeb213b5c1c05172e39ed81d0ae5d7c068e89
Yurii <chernenkoyuriy1992@gmail.com>
2022-12-08 12:03:40 +0100

    Refactor.

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,5 @@
 //import { readdirSync } from 'fs';
-import { fileURLToPath } from "url"
+import { fileURLToPath } from "url";
 import cors from "cors";
 // import { createRequire } from 'node:module';
 import { Server } from "socket.io";
@@ -9,27 +9,24 @@ import * as XLSX from "xlsx/xlsx.mjs";
 import * as fs from "fs";
 import path from "path";
 import express from "express";
-import session from "cookie-session";
+import cookieSession from "cookie-session";
 import crypto from "crypto";
 import API_V_1 from "./api/index.router.mjs";
 
-const isProd = process.env.NODE_ENV === 'production';
-const rootFolder = '../dist';
+const isProd = process.env.NODE_ENV === "production";
+const rootFolder = "../dist";
 /* load 'fs' for readFile and writeFile support */
 XLSX.set_fs(fs);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const indexFile = new URL(`${rootFolder}/index.html`, import.meta.url);
 const app = express();
 const devCors = ["http://127.0.0.1:5173", "http://localhost:5173"];
 
-app.set('trust proxy', 1) // trust first proxy
-
-if (isProd)
-{
+if (isProd) {
   app.use(express.static(path.join(__dirname, `/${rootFolder}`)));
 } else {
-  app.use(cors({
+  app.use(
+    cors({
       origin: devCors,
       credentials: true,
     })
@@ -38,23 +35,26 @@ if (isProd)
 
 // todo: update with values from env
 app.use(
-  session({
+  cookieSession({
     name: "spreadsheet-session",
-    // todo: replace with env.SESSIOB_SECRET
-    secret: "secretKeyForSession",
-    secure: false,
+    // todo: replace with env.SESSION_SECRET
+    keys: ["secretKeyForSession"],
     // Cookie Options
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
-    sameSite: "none",
+    secure: false,
+    sameSite: false,
+    httpOnly: false,
   })
 );
 
-// // register session before api
+// trust proxy
+app.set("trust proxy");
+
+// register session before api
 app.use((req, res, next) => {
-  console.log(req.session);
   // add session uid for user
   if (req.session.isNew && !req.session.sid) {
-    req.session.sid = (req.session.sid || crypto.randomUUID());
+    req.session.sid = req.session.sid || crypto.randomUUID();
   }
 
   next();
@@ -62,16 +62,12 @@ app.use((req, res, next) => {
 
 app.use("/api/v1", API_V_1);
 
-app.get("*", (req, res) => {
-  res.sendFile(fileURLToPath(indexFile));
-});
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: devCors,
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
