a4e0a2abba719a2b0cbdbd03a5dd515c67123dfc
Yurii <chernenkoyuriy1992@gmail.com>
2023-01-14 14:21:11 +0100

    Update request handling. Provide response.

--- server/utils/websocket.handler.mjs
+++ server/utils/websocket.handler.mjs
@@ -3,29 +3,43 @@ import DocumentController from "../controller/document.controller.mjs";
 
 const documentController = new DocumentController();
 
-function onFocusCell(socket, data) {
-  documentController.addFocussedCell(data.tableName, data);
-  data.selectedList = documentController.getFocussedCell(data.tableName);
-  socket.broadcast.emit(SEVENTS.CELL.FOCUSED, data);
+function onFocusCell(socket, data, callback) {
+  var result = documentController.addFocussedCell(data.tableName, data);
+
+  if (result.success) {
+    data.selectedList = documentController.getFocussedCell(data.tableName).data;
+    socket.broadcast.emit(SEVENTS.CELL.FOCUSED, data);
+  }
+
+  callback(result);
 }
 
-function onSaveCell(socket, data) {
-  // TODO: update
-  documentController.updateDocument(data.tableName, data);
-  documentController.removeFocussedCell(data.tableName, data);
-  data.selectedList = documentController.getFocussedCell(data.tableName);
-  socket.broadcast.emit(SEVENTS.CELL.SAVED, data);
+async function onSaveCell(socket, data, callback) {
+  var result = await documentController.updateDocument(data.tableName, data);
+  
+  if (result.success) {
+    documentController.removeFocussedCell(data.tableName, data);
+    data.selectedList = documentController.getFocussedCell(data.tableName).data;
+    socket.broadcast.emit(SEVENTS.CELL.SAVED, data);
+  }
+ 
+  callback(result);
 }
 
-function onSaveDocument(socket, data) {
-  documentController.saveDocument(data.tableName);
-  socket.emit(SEVENTS.DOCUMENT.SAVED, data);
+function onSaveDocument(socket, data, callback) {
+  var result = documentController.saveDocument(data.tableName);
+
+  if (result.success) {
+    socket.emit(SEVENTS.DOCUMENT.SAVED, data);
+  } 
+
+  callback(result);
 }
 
 function registerEvents(io, socket) {
-  socket.on(SEVENTS.CELL.FOCUS, (data) => onFocusCell(socket, data));
-  socket.on(SEVENTS.CELL.SAVE, (data) => onSaveCell(socket, data));
-  socket.on(SEVENTS.DOCUMENT.SAVE, (data) => onSaveDocument(socket, data));
+  socket.on(SEVENTS.CELL.FOCUS, (data, cb) => onFocusCell(socket, data, cb));
+  socket.on(SEVENTS.CELL.SAVE, (data, cb) => onSaveCell(socket, data, cb));
+  socket.on(SEVENTS.DOCUMENT.SAVE, (data, cb) => onSaveDocument(socket, data, cb));
 }
 
 export default registerEvents;
